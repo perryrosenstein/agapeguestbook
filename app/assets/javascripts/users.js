aeb9bcf10cb497ce87c37287d2ef11ffe8a9a95f
Perry Rosenstein <perryr@gmail.com>
2013-10-21 14:00:33 -0700

    add accept tos modal & checkbox tied to submit button

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,9 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(function() {
+    $('#accept-tos').click(function() {
+        if ($("#accept-tos").prop("checked")) {
+            $("#submit-button").removeClass("disabled");
+        } else {
+            $("#submit-button").addClass("disabled")
+        }
+    });
+});
