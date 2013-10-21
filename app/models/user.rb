40b4d3bb3264f573184960ad8ca7f4a75d86b7df
Perry Rosenstein <perryr@gmail.com>
2013-10-21 13:38:07 -0700

    make form pretty, add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, :presence => true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :arrival_date, :presence => true
+  validates :departure_date, :presence => true
+  validates :where_from, :presence => true
 end
