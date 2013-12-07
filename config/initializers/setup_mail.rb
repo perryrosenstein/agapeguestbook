fb62089a69b6220050c3676ab543c9e0fb5f9db5
Perry Rosenstein <perryr@gmail.com>
2013-12-06 19:52:46 -0800

    tidy up application.yml

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,15 +2,15 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
 
+# sets up rails to send emails from a gmail.com email account.
 ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => ENV['SMTP_USERNAME'],
-    :password             => ENV['SMTP_PASSWORD'],
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
 }
 
 ActionMailer::Base.default_url_options[:host] = "localhost:3000"
-#Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
