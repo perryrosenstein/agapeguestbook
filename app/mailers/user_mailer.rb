69fd0aa5e4a5c27601a36f460dbbb10082c908bc
Perry Rosenstein <perryr@gmail.com>
2013-12-10 15:13:41 -0800

    attempt to fix bug with url in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def user_created(user)
     @user = user
-    @user_url = root_url + "?user_id=#{@user.id}"
+    @user_url  = user_url(@user, host: request.host_with_port )
     mail(to: "#{ENV["USER_CREATED_ALERT_RECIPIENTS"]}", subject: "#{@user.first_name} added to Agape Guestbook!")
   end
 
